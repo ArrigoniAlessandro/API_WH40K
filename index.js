83e5a981ab87ef5a4fead658b16fb75b355d61b6
Arrigoni Alessandro <arrigoni.alessandro@osdbsesto>
2022-02-04 15:44:58 +0100

    Added Upgraded Mail

---
+++ index.js
@@ -0,0 +1,49 @@
+const { request, response, query } = require("express");
+var express = require("express"); 
+var apiServer = express();
+var fs = require ("fs");
+const { get } = require("http");
+const { setFlagsFromString } = require("v8");
+
+var port = 3000;
+var host = "localhost";
+apiServer.listen(port, host, () => {
+console.log("server running at http://%s:%d", host, port);
+});
+
+apiServer.post("/InvioMailUpgrade", (req,res) => {
+    if(req.body.mail){
+        console.log("mail:".mail);
+// carico il modulo
+const nodemailer = require('nodemailer');
+
+// definisco il trasporto
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'arrigoni.alessandro@studenti.salesianisesto.it',
+    pass: 'Az-66393'
+  }
+});
+
+// messaggio da inviare
+var mailOptions = {
+  from: 'mail di trasporto',
+  to: mail,
+  subject: "Hai effettuato l'upgrade al tuo account ",
+  html: '<b>corpo mail</b>'  // invia il corpo in html
+};
+
+// invio il messaggio
+transporter.sendMail(mailOptions, function(error, info){
+  if(error) {
+    console.log(error);
+  }else{
+    console.log('Messaggio inviato: ' + info.response);
+  }
+});
+        res.send("Done!");
+    }else{
+        res.send("Wrong data");
+    }
+    });
